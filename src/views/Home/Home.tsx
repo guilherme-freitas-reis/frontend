e8824ed5fd5085864c6a20de625aebb6e009dfbf
Lucas <lucasbarbosa@outlook.com>
2021-02-15 21:11:12 -0300

    chore: home usa novo footer

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import Banner from './components/Banner/Banner';
-import Footer from './components/Footer/Footer';
+import Footer from '../../components/Footer/Footer';
 
 import { HomeContainer } from './styles/Home.styles';
 
