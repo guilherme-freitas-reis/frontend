6f2cafc5bc7cd45d0da87f61ee88de20ad5b7c06
Lucas Barbosa <lucasbarbosa@outlook.com>
2021-02-18 19:09:46 -0300

    chore: formatting before checkout

--- src/pages/criar-conta/index.tsx
+++ src/pages/criar-conta/index.tsx
@@ -2,10 +2,14 @@ import { FC } from 'react';
 import Footer from '../../components/Footer/Footer';
 import Input from '../../components/Input/Input';
 import Navbar from '../../components/Navbar/Navbar';
-import { InputName, SignInWrapper, SignInContainer } from './styles/SignInPage.styles';
+import {
+  InputName,
+  SignInWrapper,
+  SignInContainer,
+} from './styles/SignInPage.styles';
 
 export interface ISignInPage {
-  color: string,
+  color: string;
 }
 
 const SignInPage: FC<ISignInPage> = () => (
@@ -14,27 +18,63 @@ const SignInPage: FC<ISignInPage> = () => (
     <SignInContainer>
       <div>
         <InputName>Nome completo</InputName>
-        <Input backgroundColor="#ddd" onFocusBackgroundColor="white" color="black" />
+        <Input
+          backgroundColor="#ddd"
+          onFocusBackgroundColor="white"
+          color="black"
+        />
         <InputName>E-mail</InputName>
-        <Input backgroundColor="#ddd" onFocusBackgroundColor="white" color="black" />
+        <Input
+          backgroundColor="#ddd"
+          onFocusBackgroundColor="white"
+          color="black"
+        />
         <InputName>Senha</InputName>
-        <Input backgroundColor="#ddd" onFocusBackgroundColor="white" color="black" />
+        <Input
+          backgroundColor="#ddd"
+          onFocusBackgroundColor="white"
+          color="black"
+        />
       </div>
       <div>
         <InputName>Opção</InputName>
-        <Input backgroundColor="#ddd" onFocusBackgroundColor="white" color="black" />
+        <Input
+          backgroundColor="#ddd"
+          onFocusBackgroundColor="white"
+          color="black"
+        />
         <InputName>Opção</InputName>
-        <Input backgroundColor="#ddd" onFocusBackgroundColor="white" color="black" />
+        <Input
+          backgroundColor="#ddd"
+          onFocusBackgroundColor="white"
+          color="black"
+        />
         <InputName>Opção</InputName>
-        <Input backgroundColor="#ddd" onFocusBackgroundColor="white" color="black" />
+        <Input
+          backgroundColor="#ddd"
+          onFocusBackgroundColor="white"
+          color="black"
+        />
       </div>
       <div>
         <InputName>Opção</InputName>
-        <Input backgroundColor="#ddd" onFocusBackgroundColor="white" color="black" />
+        <Input
+          backgroundColor="#ddd"
+          onFocusBackgroundColor="white"
+          color="black"
+        />
         <InputName>Opção</InputName>
-        <Input backgroundColor="#ddd" onFocusBackgroundColor="white" color="black" />
+        <Input
+          backgroundColor="#ddd"
+          onFocusBackgroundColor="white"
+          color="black"
+        />
         <InputName>Opção</InputName>
-        <Input backgroundColor="#ddd" onFocusBackgroundColor="white" color="black" />
+        <Input
+          backgroundColor="#ddd"
+          onFocusBackgroundColor="white"
+          color="black"
+        />
       </div>
     </SignInContainer>
     <Footer />
