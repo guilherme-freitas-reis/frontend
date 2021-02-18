63cf29d37254d2c9a685e288b18a24e7e9f0af10
Guilherme de Freitas Reis <guilhermereis9876@gmail.com>
2021-02-18 20:18:32 -0300

    chore: componentizando formulario

---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link';
+import { FC } from 'react';
+import { IoMailOutline, IoKeyOutline } from 'react-icons/io5';
+import Button from '../../components/Button/Button';
+import Input from '../../components/Input/Input';
+import { Container, LogoImage, SignUpText } from './styles/LoginForm.styles';
+
+const LoginForm: FC = () => (
+  <Container>
+    <Link href="/">
+      <LogoImage
+        src="/images/localiza-logo-tp.png"
+        alt="localiza-logo"
+        height="35px"
+      />
+    </Link>
+    <Input
+      type="text"
+      color="black"
+      mask="cpf"
+      backgroundColor="#eee"
+      onFocusBackgroundColor="white"
+      placeholder="Digite seu CPF..."
+      placeholderColor="gray"
+      iconBefore={<IoMailOutline size={20} />}
+    />
+    <Input
+      type="password"
+      color="black"
+      backgroundColor="#eee"
+      onFocusBackgroundColor="white"
+      placeholder="Digite sua senha..."
+      placeholderColor="gray"
+      iconBefore={<IoKeyOutline size={20} />}
+    />
+    <Button block>Fazer Login</Button>
+    <SignUpText>
+      <h2>Ainda não tem uma conta?</h2>
+      <Link href="/criar-conta">Cadastre-se.</Link>
+    </SignUpText>
+  </Container>
+);
+
+export default LoginForm;
--- src/views/Login/LoginPage.tsx
+++ src/views/Login/LoginPage.tsx
@@ -1,14 +1,8 @@
-import Link from 'next/link';
 import { FC } from 'react';
-import { IoMailOutline, IoKeyOutline } from 'react-icons/io5';
-import Button from '../../components/Button/Button';
-import Input from '../../components/Input/Input';
+import LoginForm from '../../components/LoginForm/LoginForm';
 import Layout from '../Layout/Layout';
 import {
-  LoginForm,
   LoginPageWrapper,
-  LogoImage,
-  SignUpText,
 } from './styles/LoginPage.styles';
 
 interface ILoginPage {
@@ -18,40 +12,7 @@ interface ILoginPage {
 const LoginPage: FC<ILoginPage> = () => (
   <Layout>
     <LoginPageWrapper className="container">
-      <LoginForm>
-        <Link href="/">
-          <LogoImage
-            src="/images/localiza-logo-tp.png"
-            alt="localiza-logo"
-            height="35px"
-          />
-        </Link>
-        {/* <h1>Fazer login</h1> */}
-        <Input
-          type="text"
-          color="black"
-          mask="cpf"
-          backgroundColor="#eee"
-          onFocusBackgroundColor="white"
-          placeholder="Digite seu CPF..."
-          placeholderColor="gray"
-          iconBefore={<IoMailOutline size={20} />}
-        />
-        <Input
-          type="password"
-          color="black"
-          backgroundColor="#eee"
-          onFocusBackgroundColor="white"
-          placeholder="Digite sua senha..."
-          placeholderColor="gray"
-          iconBefore={<IoKeyOutline size={20} />}
-        />
-        <Button block>Fazer Login</Button>
-        <SignUpText>
-          <h2>Ainda não tem uma conta?</h2>
-          <Link href="/criar-conta">Cadastre-se.</Link>
-        </SignUpText>
-      </LoginForm>
+      <LoginForm />
     </LoginPageWrapper>
   </Layout>
 );
