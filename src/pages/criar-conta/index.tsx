63f00c215bba523cdca04a0f61d5003882eac5e2
Lucas Barbosa <lucasbarbosa@outlook.com>
2021-02-19 00:22:55 -0300

    chore: implementação pagina cadastro

--- src/pages/criar-conta/index.tsx
+++ src/pages/criar-conta/index.tsx
@@ -1,4 +1,8 @@
 import { FC } from 'react';
+import {
+  IoDocumentTextOutline, IoHappyOutline, IoKeyOutline, IoMailOutline, IoPhonePortraitOutline,
+} from 'react-icons/io5';
+import Button from '../../components/Button/Button';
 import Footer from '../../components/Footer/Footer';
 import Input from '../../components/Input/Input';
 import Navbar from '../../components/Navbar/Navbar';
@@ -16,66 +20,66 @@ const SignInPage: FC<ISignInPage> = () => (
   <SignInWrapper>
     <Navbar />
     <SignInContainer>
+      <h1>Fazer cadastro</h1>
       <div>
-        <InputName>Nome completo</InputName>
+        <InputName>Nome</InputName>
         <Input
-          backgroundColor="#ddd"
+          iconBefore={<IoHappyOutline size={20} />}
+          backgroundColor="#f0f0f0"
           onFocusBackgroundColor="white"
           color="black"
         />
         <InputName>E-mail</InputName>
         <Input
-          backgroundColor="#ddd"
+          type="email"
+          iconBefore={<IoMailOutline size={20} />}
+          backgroundColor="#f0f0f0"
           onFocusBackgroundColor="white"
           color="black"
         />
         <InputName>Senha</InputName>
         <Input
-          backgroundColor="#ddd"
+          type="password"
+          iconBefore={<IoKeyOutline size={20} />}
+          backgroundColor="#f0f0f0"
           onFocusBackgroundColor="white"
           color="black"
         />
       </div>
       <div>
-        <InputName>Opção</InputName>
+        <InputName>Sobrenome</InputName>
         <Input
-          backgroundColor="#ddd"
+          iconBefore={<IoHappyOutline size={20} />}
+          backgroundColor="#f0f0f0"
           onFocusBackgroundColor="white"
           color="black"
         />
-        <InputName>Opção</InputName>
+        <InputName>Telefone</InputName>
         <Input
-          backgroundColor="#ddd"
-          onFocusBackgroundColor="white"
-          color="black"
-        />
-        <InputName>Opção</InputName>
-        <Input
-          backgroundColor="#ddd"
+          iconBefore={<IoPhonePortraitOutline size={20} />}
+          mask="telefone"
+          backgroundColor="#f0f0f0"
           onFocusBackgroundColor="white"
           color="black"
         />
       </div>
       <div>
-        <InputName>Opção</InputName>
+        <InputName>Gênero</InputName>
         <Input
-          backgroundColor="#ddd"
+          backgroundColor="#f0f0f0"
           onFocusBackgroundColor="white"
           color="black"
         />
-        <InputName>Opção</InputName>
+        <InputName>CPF</InputName>
         <Input
-          backgroundColor="#ddd"
-          onFocusBackgroundColor="white"
-          color="black"
-        />
-        <InputName>Opção</InputName>
-        <Input
-          backgroundColor="#ddd"
+          iconBefore={<IoDocumentTextOutline size={20} />}
+          mask="cpf"
+          backgroundColor="#f0f0f0"
           onFocusBackgroundColor="white"
           color="black"
         />
       </div>
+      <Button background="#ddaa00" type="submit">Cadastrar</Button>
     </SignInContainer>
     <Footer />
   </SignInWrapper>
