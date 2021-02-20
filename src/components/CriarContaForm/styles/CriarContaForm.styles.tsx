feb04166f93418b98744ef50883b250c1403d588
Lucas <lucasbarbosa@outlook.com>
2021-02-19 23:38:11 -0300

    chore: implementação endereço cadastro, estilos

--- src/components/CriarContaForm/styles/CriarContaForm.styles.tsx
+++ src/components/CriarContaForm/styles/CriarContaForm.styles.tsx
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
+export const FormWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 20px;
+`;
+
 export const Form = styled.form`
   display: flex;
   align-items: center;
@@ -11,16 +17,17 @@ export const Form = styled.form`
   box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
   min-width: 450px;
 
-  h1 {
-    color: #00a85a;
-    font-size: 1.8rem;
-    font-weight: bold;
-    margin: 1rem 0 2rem;
-  }
-
   button {
     margin: 1rem 0 .8rem;
   }
+`;
+
+export const SignInTitle = styled.p`
+  color: #00a85a;
+  font-size: 1.8rem;
+  font-weight: bold;
+  margin: 1rem 0 2rem;
+  grid-column: 1 / 3;
 `;
 
 export const SignUpText = styled.div`
